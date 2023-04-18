7b67fce3b708a906cd48f49a382f9da41b07cb46
Mateus Cristófori <mateuscristofori.gomes@gmail.com>
2023-04-18 10:13:45 -0500

    Studing clean architecture

---
+++ src/route.entity.ts
@@ -0,0 +1,49 @@
+// * Esse arquivo representa a "parte amarela" do anel concêntrico, ou seja, é parte que irá concentrar as regras de negócio da aplicação.
+// * As regras de negócio deve ser agnóstico a framework e tecnologias!
+
+export type RouteProps = {
+  title: string;
+  startPosition: Coordenates;
+  endPosition: Coordenates;
+  points?: Coordenates[];
+};
+
+export type Coordenates = {
+  lat: number;
+  long: number;
+};
+
+export class Route {
+  // * Primeira regra da classe. Criação de uma rota. Decisões de armazenameto não importam diretamente para a entidade. Entidade deve ser totalmente acoplada.
+  // * A lib que deve ser independente a regra de negócio e não o contrário. Com isso, poderemos ter uma maior flexibilidade de uso de diferentes tecnologias sem afetar as regras de negócio. Os serviços devem ser desacoplados.
+  public props: Required<RouteProps>;
+  constructor(props: RouteProps) {
+    this.props = {
+      ...props,
+      points: props.points || [],
+    };
+  }
+
+  // * Esse método representa uma intenção de mudança de algo que faz parte da entidade! Não é um método "anêmico", mas representa um comportamento.
+  // * Devemos separar as alterações de cada atributo para métodos diferentes! Sempre respeitar a responsabilidade única.
+  updateTitle(title: string) {
+    this.props.title = title;
+  }
+
+  // ? Esse método precisa ser necessariamente privado, ou temos a opção de não criá-lo para evitar falta de expressividade da entidade?
+  private set title(title: string) {
+    this.props.title = title;
+  }
+}
+
+const rota = new Route({
+  title: "Rota",
+  startPosition: { lat: 15, long: 15 },
+  endPosition: { lat: 20, long: 20 },
+  points: [
+    { lat: 20, long: 20 },
+    { lat: 20, long: 20 },
+  ],
+});
+// * Com o frameworks, temos o conceito de "entidades anêmicas". Que é sempre usar métodos de acesso para tomar ações dentro da classe, não deixar as regras de negócio de forma clara na entidade.
+// * Todas as mudanças na entidade devem ser feitas via métodos que representem comportamentos de mudança. Isso evita a "anemia" ou, "falta de expressividade", da entidade. Ao invés de usar um "setTitle()", usa-se o "updateTitle()". Esses métodos de comportamento representam uma *intenção* de mudança.
